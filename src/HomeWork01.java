b710dc880c520aba86324b3ae0fba55568ecdd44
“DraceaDumitru” <denys97ddd@gmail.com>
2020-12-09 12:26:42 +0200

    tema

---
+++ src/HomeWork01.java
@@ -0,0 +1,19 @@
+public class HomeWork01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!"); //ex 1
+        System.out.println("Dan");
+        int sum = 5 + 3 ;                  // ex 2
+        System.out.println(sum);
+        int div = 6/3 ;                    //ex 3
+        System.out.println(div);
+        int a = -5 + 8 * 6 ;               //ex4
+        System.out.println(a);
+        double b = (55+9) % 9 ;
+        System.out.println(b);
+        double c = 20 + -3*5 / 8 ;
+        System.out.println(c);
+        double d = 5 + 15 / 3 * 2 - 8 % 3 ;
+        System.out.println(d);
+    }
+
+}
